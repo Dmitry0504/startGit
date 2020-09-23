a5cb1343b58363dc9c127cba9822fd7e39dda478
”Junior” <rip05-04-1990@yandex.ru>
2020-09-23 18:28:44 +0400

    add loop print

--- Main.java
+++ Main.java
@@ -4,7 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello world!!");
-        System.out.println("Hello world!! Again...");
+        for(int i = 5; i > 0; i--){
+        System.out.println("Hello world!!");}
+        
+
     }
 }
